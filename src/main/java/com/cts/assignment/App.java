8df6fac46a263864876b36c715c5cd7537218f57
arpana-patle <arpana.patle@cognizant.com>
2024-03-16 13:10:55 +0530

    Update App.java

--- src/main/java/com/cts/assignment/App.java
+++ src/main/java/com/cts/assignment/App.java
@@ -4,35 +4,24 @@ import java.util.ArrayList;
 
 import com.cts.assignment.model.*;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-      
-        /* create FDAccount maturityDate as LocalDate
-        * interestRate as double
-        * and create below methods
-        * getTenure - to return the tenure of the FD in months with list object  print result
-            */
-        FDAccount fDAccount = new FDAccount(2, 100000, java.time.LocalDate.now().plusMonths(12), 0.05,new ArrayList<Transaction>());
-        System.out.println(fDAccount.getTenure());
-        System.out.println(fDAccount.getMaturityAmount());
-        System.out.println(fDAccount.getAccountDetails());
+public class App {
+	public static void main(String[] args) {
 
-        // create account SBAccount print result
-        SBAccount sBAccount = new SBAccount(1, 500000, 1000, new ArrayList<Transaction>());
-        try {
-            sBAccount.withdraw(100000);
-        } catch (com.cts.assignment.exception.InsufficientBalanceException e) {
-            System.out.println(e.getMessage());
-        }
-        System.out.println(sBAccount.getAccountDetails());
+		// create FD Account
+		FDAccount fDAccount = new FDAccount(2, 100000, java.time.LocalDate.now().plusMonths(12), 0.05,
+				new ArrayList<Transaction>());
+		System.out.println(fDAccount.getTenure());
+		System.out.println(fDAccount.getMaturityAmount());
+		System.out.println(fDAccount.getAccountDetails());
 
+		// create SB Account
+		SBAccount sBAccount = new SBAccount(1, 500000, 1000, new ArrayList<Transaction>());
+		try {
+			sBAccount.withdraw(100000);
+		} catch (com.cts.assignment.exception.InsufficientBalanceException e) {
+			System.out.println(e.getMessage());
+		}
+		System.out.println(sBAccount.getAccountDetails());
 
-        
-    }
+	}
 }

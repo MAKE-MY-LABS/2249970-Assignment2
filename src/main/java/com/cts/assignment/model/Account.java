1ac7a7215ab0b9c72089cf0aa07bccfc08d8cb29
arpana-patle <arpana.patle@cognizant.com>
2024-03-16 13:17:39 +0530

    Update Account.java

--- src/main/java/com/cts/assignment/model/Account.java
+++ src/main/java/com/cts/assignment/model/Account.java
@@ -2,8 +2,6 @@ package com.cts.assignment.model;
 
 import java.util.List;
 
-import com.cts.assignment.exception.*;
-
 /*
  * Create an abstract class Account with the following fields:
  * accountNumber as integer
@@ -16,60 +14,55 @@ import com.cts.assignment.exception.*;
  * getAccountDetails method which returns a string with account details
  */
 public abstract class Account {
-    private int accountNumber;
-    private double balance;
-    private java.time.LocalDate openingDate;
-    private List<Transaction> transactions;
+	private int accountNumber;
+	private double balance;
+	private java.time.LocalDate openingDate;
+	private List<Transaction> transactions;
 
-    public Account(int accountNumber, double balance) {
-        this.accountNumber = accountNumber;
-        this.balance = balance;
-        this.openingDate = java.time.LocalDate.now();
-    }
+	public Account(int accountNumber, double balance) {
+		this.accountNumber = accountNumber;
+		this.balance = balance;
+		this.openingDate = java.time.LocalDate.now();
+	}
 
-    //setter
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
 
+	public int getAccountNumber() {
+		return this.accountNumber;
+	}
 
-    public int getAccountNumber() {
-        return this.accountNumber;
-    }
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
 
-    //setter
-    public void setAccountNumber(int accountNumber) {
-        this.accountNumber = accountNumber;
-    }
+	public double getBalance() {
+		return this.balance;
+	}
 
-    public double getBalance() {
-        return this.balance;
-    }
+	public java.time.LocalDate getOpeningDate() {
+		return this.openingDate;
+	}
 
-    public java.time.LocalDate getOpeningDate() {
-        return this.openingDate;
-    }
+	public List<Transaction> getTransactions() {
+		return this.transactions;
+	}
 
-    public List<Transaction> getTransactions() {
-        return this.transactions;
-    }
+	public void setTransactions(List<Transaction> transactions) {
+		this.transactions = transactions;
+	}
 
-    //setter
-    public void setTransactions(List<Transaction> transactions) {
-        this.transactions = transactions;
-    }
+	public void deposit(double amount) {
+		this.balance += amount;
+		Transaction transaction = new Transaction.Builder().type(Transaction.Type.CREDIT).amount(amount).build();
+		this.transactions.add(transaction);
+	}
 
-    public void deposit(double amount) {
-        this.balance += amount;
-        Transaction transaction = new Transaction.Builder().type(Transaction.Type.CREDIT).amount(amount).build();
-        this.transactions.add(transaction);
-    }
+	public abstract String getAccountDetails();
 
-    public abstract String getAccountDetails();
-
-    // tostring
-    public String toString() {
-        return "Account Number: " + this.getAccountNumber() + ", Balance: " + this.getBalance() + ", Opening Date: "
-                + this.getOpeningDate();
-    }
+	public String toString() {
+		return "Account Number: " + this.getAccountNumber() + ", Balance: " + this.getBalance() + ", Opening Date: "
+				+ this.getOpeningDate();
+	}
 }
